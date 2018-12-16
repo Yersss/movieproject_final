34429494e36c8ba4e64f93212884d879ad99cf48
AlmasSapar <YOUR@EMAIL.com>
2018-12-17 03:55:43 +0600

    feat(templates/register.html, templates/base.html, user/urls.py): created basic pages, added urls in user

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    movieid = models.CharField(max_length=20, primary_key=True)
+    title = models.CharField(max_length=30)
+    year = models.CharField(max_length=4)
+    genres = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.movieid + '|' + self.title
+
+    @staticmethod
+    def get_name():
+        return 'movie'
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+
 
 # Create your views here.
+@csrf_protect
+def detail(request, model, id):
+    items = []
+    try:
+        if model.get_name() == 'movie' and id != 'None':
+            try:
+                d = Popularity.objects.get(movieid_id=id)
+                weight = d.weight
+                d.delete()
+                new_record = Popularity(movieid_id=id, weight=weight + 1)
+                new_record.save()
+            except:
+                new_record = Popularity(movieid_id=id, weight=1)
+                new_record.save()
+            label = 'actor'
+            object = model.objects.get(movieid=id)
+            records = Act.objects.filter(movieid_id=id)
+            if request.user.get_username() != '':
+                seen_list = [str(x).split('|')[1] for x in
+                             Seen.objects.filter(username=request.user.get_username())]
+                expect_list = [str(y).split('|')[1] for y in
+                               Expect.objects.filter(username=request.user.get_username())]
+                if id in seen_list:
+                    object.flag = 1
+                if id in expect_list:
+                    object.flag = 2
+            for query in records:
+                for actor in Actor.objects.filter(actorid=query.actorid_id):
+                    items.append(actor)
+    except:
+        return render(request, '404.html')
+    return render(request, '{}_list.html'.format(label), {'items': items, 'number': len(items), 'object': object})
